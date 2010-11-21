264e1e6e7649baa1c4ba157b2de065e91122962f
Jelle Mattez <info@blackweb.be>
2010-11-21 22:38:39 +0100

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title
+    base_title = "Ruby on Rails"
+    end_title = " | by Jelle Mattez"
+    if @title.nil?
+      "#{base_title}#{end_title}"
+    else
+       "#{base_title} | #{@title}#{end_title}"
+    end
+  end
 end
